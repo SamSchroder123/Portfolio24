63db8cba3c6dd87bd5d520e2af9298cb3389c75f
SamSchroder123 <schrodersamuel9@gmail.com>
2024-11-19 15:26:50 +0000

    functional routing for project pages

--- index.js
+++ index.js
@@ -6,23 +6,28 @@ const slugify = require("slugify");
 const express = require("express");
 const path = require("path");
 
-function getProjectPage() {}
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const app = express();
-
-const replaceTemplate = require("./modules/replaceTemplate");
 
 const data = fs.readFileSync(`${__dirname}/projects.json`, "utf-8");
 const dataObject = JSON.parse(data);
 console.log(dataObject);
 const projectsArr = dataObject.projects;
 
+let projectSlugsArr = [];
+for (let i = 0; i < projectsArr.length; i++) {
+  projectSlugsArr[i] = slugify(projectsArr[i].name);
+}
 const home = fs.readFileSync(`${__dirname}/templates/index.html`, "utf-8");
 const projectCard = fs.readFileSync(
   `${__dirname}/templates/projectCard.html`,
   "utf-8"
 );
-
+const projectPage = fs.readFileSync(
+  `${__dirname}/templates/projectPage.html`,
+  "utf-8"
+);
 // Serve static files from the 'images' and 'styling' folders
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/styling", express.static(path.join(__dirname, "styling")));
@@ -45,6 +50,19 @@ app.get("/", (req, res) => {
   console.log(projectsHtml);
   const output = home.replace("{%PROJECTS%}", projectsHtml);
   res.end(output);
+});
+
+app.get("/project/*", (req, res) => {
+  res.writeHead(200, { "content-type": "text/html" });
+  let projectRequested = req.url.slice(
+    req.url.lastIndexOf("/") + 1,
+    req.url.length
+  );
+  if (projectSlugsArr.includes(projectRequested)) {
+    projectIndex = projectSlugsArr.indexOf(projectRequested);
+    const outputHtml = replaceTemplate(projectPage, projectsArr[projectIndex]);
+    res.end(outputHtml);
+  }
 });
 
 // const server = http.createServer((req, res) => {
